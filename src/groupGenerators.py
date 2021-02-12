83c79e5447b636d477c3d21bf32e7415e4924761
evnor <paul.vdh@hotmail.com>
2021-02-12 10:45:07 +0100

    Split off generateGroups

--- src/groupGenerators.py
+++ src/groupGenerators.py
@@ -1,4 +1,8 @@
- #Hello
-def spam():
-    pass
+from typing import List
+import random
 
+def generate_groups(time: int) -> List[int]:
+    """
+    Generates the groups to be added to the line.
+    """
+    return [random.randint(1,7) for _ in range(random.randint(1,2))]
