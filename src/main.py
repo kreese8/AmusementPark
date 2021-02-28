2245ee08a4d95799afe62ed4e9e16af5047c4357
evnor <paul.vdh@hotmail.com>
2021-02-28 10:54:51 +0100

    Meeting 26 feb

--- src/main.py
+++ src/main.py
@@ -2,38 +2,29 @@
 This is merely a quick and dirty example. It doesn't provide any support for
 customisation, extending functionality, or more extensive data collection.
 """
-
 from typing import List
 import groupGenerators as gen
 import random
+    
 
-def load_boat(line: List[int]) -> List[int]:
+def generate_groups(time: int) -> List[int]:
+    """Generates the groups to be added to the line.
     """
-    Tries to load a single boat with the groups in the line and the solo_groups.
-    Returns the new line, and the new amount of groups.
+    return [random.randint(1,7) for _ in range(random.randint(1,2))]
+
+def step_time(line: List[int], time: int) -> List[int]:
+    """Generates new groups and loads a single boat.
     """
+    # People arrive
+    line.extend(gen.generate_groups(time))
+    # Load the boat
     remaining = 8
     while len(line) > 0 and remaining >= line[0]:
         remaining -= line.pop(0)
     return line
 
-def generate_groups(time: int) -> List[int]:
-    """
-    Generates the groups to be added to the line.
-    """
-    return [random.randint(1,7) for _ in range(random.randint(1,2))]
-
-def step_time(line: List[int], time: int) -> List[int]:
-    """
-    Generates new groups and loads a single boat.
-    """
-    line.extend(gen.generate_groups(time))
-    line = load_boat(line)
-    return line
-
 def perf_timesteps(n: int)->None:
-    """
-
+    """Step forward in time n steps
     """
     line = []
     for time in range(n):
